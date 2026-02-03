f1946d7b5b45b5116c5c522d69218ccb67ece4fd
Nurtal <nathan.foulquier.pro@gmail.com>
2026-02-03 17:00:50 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+
+    // tadam 
+    let i:i32 = 22;
+    let nombres: Vec<i32> = vec![10, 20, 30];
+    
+    // intro
+    println!("Hello Jimmy du {}, ça va faire un bail qu'on a pas fait de language compilé hein ? ", i);
+}
