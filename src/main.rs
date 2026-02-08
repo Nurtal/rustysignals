2589bd35a42a4b462030452fe94102574c9a5cd6
Nurtal <nathan.foulquier.pro@gmail.com>
2026-02-08 18:23:01 +0100

    FFT

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rustfft::{FftPlanner, num_complex::Complex};
+
 
 // fonction test pour calculer la somme d'un vecteur
 fn sum_vec(xs: &[i32]) -> i32 {
@@ -39,11 +41,30 @@ fn derive(signal: &[i32]) -> Vec<f32> {
 
 }
 
+// Run une FFT 
+fn fft_1d(signal: &[f32]) -> Vec<Complex<f32>> {
+    let mut planner = FftPlanner::<f32>::new();
+    let fft = planner.plan_fft_forward(signal.len());
+
+    // Copie du signal réel dans un buffer complexe
+    let mut buffer: Vec<Complex<f32>> = signal
+        .iter()
+        .map(|&x| Complex { re: x, im: 0.0 })
+        .collect();
+
+    // Calcul FFT
+    fft.process(&mut buffer);
+
+    buffer
+}
+
+
 fn main() {
 
     // tadam 
     let i:i32 = 22;
     let nombres: Vec<i32> = vec![10, 20, 30, 20, 10];
+    let signal = vec![0.0, 1.0, 0.0, -1.0];
     
     // intro
     println!("Hello Jimmy du {}, ça va faire un bail qu'on a pas fait de language compilé hein ? ", i);
@@ -69,6 +90,19 @@ fn main() {
     let deriv = derive(&nombres);
     println!("On peut faire une dérivée {:?}", deriv);
 
+    // FFT
+    let spectrum = fft_1d(&signal);
+    println!("calcul d'un FFT")
+    for (i, c) in spectrum.iter().enumerate() {
+        println!(
+            "bin {:>2} | re = {:>6.3}, im = {:>6.3}, magnitude = {:>6.3}",
+            i,
+            c.re,
+            c.im,
+            c.norm()
+        );
+    }
 
-    // calcul de la dérivée (pas de temps != 1)
+    // Affichage du spectrogam
+
 }
